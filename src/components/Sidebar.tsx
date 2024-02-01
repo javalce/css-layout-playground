8b186a7036f38b85b7616da32605a0f819ded297
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-01 13:20:49 +0100

    Add Tabs component to Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,11 @@
 import { capitalizeFirstLetter } from '@/lib/utils';
 import { LAYOUT_TYPES } from '@/models/layout';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
 
 export default function Sidebar() {
   return (
-    <aside className='flex flex-col [grid-area:options]'>
+    <aside className='flex flex-col gap-y-4 [grid-area:options]'>
       <section>
         <Select defaultValue={LAYOUT_TYPES.FLEXBOX}>
           <SelectTrigger className='w-[180px]'>
@@ -19,6 +20,18 @@ export default function Sidebar() {
           </SelectContent>
         </Select>
       </section>
+      <Tabs className='w-[400px]' defaultValue='container'>
+        <TabsList>
+          <TabsTrigger value='container'>Container</TabsTrigger>
+          <TabsTrigger value='item'>Item</TabsTrigger>
+        </TabsList>
+        <TabsContent value='container'>
+          <div>Container css options</div>
+        </TabsContent>
+        <TabsContent value='item'>
+          <div>Item css options</div>
+        </TabsContent>
+      </Tabs>
     </aside>
   );
 }
