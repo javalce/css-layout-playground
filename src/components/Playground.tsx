1ee7a66c52c34ce2ff454b8e587923d95f28438f
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-04 17:56:09 +0100

    Add flexbox properties to Playground component

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,7 +1,19 @@
+'use client';
+
+import { useFlexbox } from '@/hooks/flexbox.hook';
+
 export default function Playground() {
+  const { flexboxDirection, flexboxWrap, flexboxJustifyContent, flexboxAlignItems } = useFlexbox();
+
   return (
     <main className='[grid-area:playground]'>
       <div>Playground</div>
+      <div>
+        <div>FlexboxDirection: {flexboxDirection}</div>
+        <div>FlexboxWrap: {flexboxWrap}</div>
+        <div>FlexboxJustifyContent: {flexboxJustifyContent}</div>
+        <div>FlexboxAlignItems: {flexboxAlignItems}</div>
+      </div>
     </main>
   );
 }
