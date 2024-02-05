54e632fe32a9e9afb7efaa62cd654aa864afd426
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-05 12:24:27 +0100

    Add Item component and render multiple items in Playground

---
+++ src/components/Item.tsx
@@ -0,0 +1,22 @@
+import { cn } from '@/lib/utils';
+import { useContainerStore } from '@/store/container.store';
+
+export default function Item({ index }: { index: number }) {
+  const selectedItemIndex = useContainerStore((state) => state.selectedItemIndex);
+  const selectItem = useContainerStore((state) => state.selectItem);
+
+  const handleClick = () => {
+    selectItem(index);
+  };
+
+  return (
+    <div
+      className={cn('size-20 bg-gray-200 text-gray-800', {
+        'border-2 border-blue-500': selectedItemIndex === index,
+      })}
+      onClick={handleClick}
+    >
+      {index}
+    </div>
+  );
+}
--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,19 +1,26 @@
 'use client';
 
 import { useFlexbox } from '@/hooks/flexbox.hook';
+import { useContainerStore } from '@/store/container.store';
+import Item from './Item';
 
 export default function Playground() {
   const { flexboxDirection, flexboxWrap, flexboxJustifyContent, flexboxAlignItems } = useFlexbox();
+  const numItems = useContainerStore((state) => state.numItems);
 
   return (
-    <main className='[grid-area:playground]'>
-      <div>Playground</div>
-      <div>
-        <div>FlexboxDirection: {flexboxDirection}</div>
-        <div>FlexboxWrap: {flexboxWrap}</div>
-        <div>FlexboxJustifyContent: {flexboxJustifyContent}</div>
-        <div>FlexboxAlignItems: {flexboxAlignItems}</div>
-      </div>
+    <main
+      className='flex gap-10 [grid-area:playground]'
+      style={{
+        flexWrap: flexboxWrap,
+        flexDirection: flexboxDirection,
+        justifyContent: flexboxJustifyContent,
+        alignItems: flexboxAlignItems,
+      }}
+    >
+      {Array.from({ length: numItems }, (_, index) => (
+        <Item key={index} index={index} />
+      ))}
     </main>
   );
 }
