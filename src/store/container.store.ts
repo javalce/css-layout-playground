8ca131786e9fa9e3f16360cc347662898c2f9313
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-05 12:15:17 +0100

    Add container store for managing items

---
+++ src/store/container.store.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+
+interface ContainerStore {
+  numItems: number;
+  selectedItemIndex: number;
+  selectItem: (index: number) => void;
+  addItem: () => void;
+  removeItem: () => void;
+}
+
+export const useContainerStore = create<ContainerStore>()((set, get) => ({
+  numItems: 0,
+  selectedItemIndex: -1,
+  selectItem: (index) => set({ selectedItemIndex: index }),
+  addItem: () => {
+    const { numItems, selectedItemIndex } = get();
+
+    if (selectedItemIndex === -1) {
+      return;
+    }
+
+    const newNumItems = numItems + 1;
+
+    set({ numItems: newNumItems });
+  },
+  removeItem: () => {
+    const { numItems, selectedItemIndex } = get();
+
+    if (selectedItemIndex === -1) {
+      return;
+    }
+
+    const newNumItems = numItems - 1;
+
+    set({ numItems: newNumItems, selectedItemIndex: -1 });
+  },
+}));
