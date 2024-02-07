caa583e9eeaf5d3a27ad859d35318766fef280b3
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-07 20:04:30 +0100

    Refactor FlexboxItemOptions component and OptionsInput component

--- src/components/FlexboxItemOptions.tsx
+++ src/components/FlexboxItemOptions.tsx
@@ -3,38 +3,51 @@
 import { useContainer } from '@/hooks/container.hook';
 import { useItem } from '@/hooks/item.hook';
 import { FLEXBOX_DEFAULTS } from '@/models/defaults';
+import type { FlexboxItemProperty } from '@/models/layout';
 import { FLEXBOX_ALIGN_SELF, FLEXBOX_ITEM_PROPERTIES } from '@/models/layout';
+import type { FlexboxItem } from '@/store/flexbox.store';
 import { OptionsContainer } from './OptionsContainer';
 import { OptionsInput } from './OptionsInput';
 import { OptionsSelector } from './OptionsSelector';
 
 export function FlexboxItemOptions() {
   const { selectedItemIndex } = useContainer();
-  const { getFlexboxItem } = useItem();
+  const { getFlexboxItem, updateFlexboxItem } = useItem();
 
   if (selectedItemIndex === -1) return null;
 
   const { order, flexGrow, flexShrink, alignSelf } = getFlexboxItem({ index: selectedItemIndex });
 
+  const handleUpdate = (property: FlexboxItemProperty, value: Partial<FlexboxItem>) => {
+    updateFlexboxItem({
+      index: selectedItemIndex,
+      propertyName: property,
+      property: value,
+    });
+  };
+
   return (
     <OptionsContainer>
       <OptionsInput
-        defaultValue={order}
         min={FLEXBOX_DEFAULTS.order}
         property={FLEXBOX_ITEM_PROPERTIES.order}
         title='Order'
+        value={order}
+        onChange={handleUpdate}
       />
       <OptionsInput
-        defaultValue={flexGrow}
         min={FLEXBOX_DEFAULTS.flexGrow}
         property={FLEXBOX_ITEM_PROPERTIES.flexGrow}
         title='Flex Grow'
+        value={flexGrow}
+        onChange={handleUpdate}
       />
       <OptionsInput
-        defaultValue={flexShrink}
         min={FLEXBOX_DEFAULTS.flexShrink}
         property={FLEXBOX_ITEM_PROPERTIES.flexShrink}
         title='Flex Shrink'
+        value={flexShrink}
+        onChange={handleUpdate}
       />
       <OptionsSelector
         data={FLEXBOX_ALIGN_SELF}
--- src/components/OptionsInput.tsx
+++ src/components/OptionsInput.tsx
@@ -1,35 +1,31 @@
 'use client';
 
-import { useItem } from '@/hooks/item.hook';
 import type { FlexboxItemProperty } from '@/models/layout';
+import type { FlexboxItem } from '@/store/flexbox.store';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
 
 export function OptionsInput({
-  defaultValue,
+  value,
   min,
-  property,
   title,
+  property,
+  onChange,
 }: {
-  defaultValue: number;
+  value: number;
   min: number;
   property: FlexboxItemProperty;
   title: string;
+  onChange: (property: FlexboxItemProperty, value: Partial<FlexboxItem>) => void;
 }) {
-  const { updateFlexboxItem } = useItem();
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateFlexboxItem({
-      index: 0,
-      propertyName: property,
-      property: { [property]: Number(e.currentTarget.value) },
-    });
+    onChange(property, { [property]: Number(e.currentTarget.value) });
   };
 
   return (
     <article>
       <Label>{title}</Label>
-      <Input defaultValue={defaultValue} min={min} type='number' onChange={handleChange} />
+      <Input min={min} type='number' value={value} onChange={handleChange} />
     </article>
   );
 }
