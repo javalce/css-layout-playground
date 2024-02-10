aa9a13ae85f35ab6a54476221bdf568a716f97b7
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-10 14:00:50 +0100

    Add layout provider and store

---
+++ src/providers/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { createLayoutStore, type LayoutStore, type LayoutStoreApi } from '@/store/layout.store';
+import { createContext, useContext, useRef } from 'react';
+import { useStore as useZustandStore } from 'zustand';
+
+const LayoutStoreContext = createContext<LayoutStoreApi | null>(null);
+
+export function LayoutStoreProvider({ children }: { children: React.ReactNode }) {
+  const storeRef = useRef<LayoutStoreApi>();
+
+  if (!storeRef.current) {
+    storeRef.current = createLayoutStore();
+  }
+
+  return (
+    <LayoutStoreContext.Provider value={storeRef.current}>{children}</LayoutStoreContext.Provider>
+  );
+}
+
+export function useStore<T>(selector: (store: LayoutStore) => T) {
+  const store = useContext(LayoutStoreContext);
+
+  if (!store) {
+    throw new Error('useStore must be used within a LayoutStoreProvider');
+  }
+
+  return useZustandStore(store, selector);
+}
---
+++ src/store/layout.store.ts
@@ -0,0 +1,135 @@
+import {
+  DEFAULT_NUM_ITEMS,
+  DEFAULT_SELECTED_ITEM_INDEX,
+  FLEXBOX_DEFAULTS,
+} from '@/models/defaults';
+import type {
+  FlexboxAlign,
+  FlexboxAlignSelf,
+  FlexboxDirection,
+  FlexboxItem,
+  FlexboxItemProperty,
+  FlexboxJustify,
+  FlexboxWrap,
+} from '@/models/layout';
+import { createStore } from 'zustand';
+
+export interface LayoutContainerState {
+  numItems: number;
+  selectedItemIndex: number;
+}
+
+export interface FlexboxLayoutContainerState {
+  flexDirection: FlexboxDirection;
+  flexWrap: FlexboxWrap;
+  justifyContent: FlexboxJustify;
+  alignItems: FlexboxAlign;
+}
+
+export interface FlexboxItemLayoutState {
+  order: number[];
+  flexGrow: number[];
+  flexShrink: number[];
+  alignSelf: FlexboxAlignSelf[];
+}
+
+export type LayoutState = LayoutContainerState &
+  FlexboxLayoutContainerState &
+  FlexboxItemLayoutState;
+
+export interface LayoutActions {
+  selectItem: (index: number) => void;
+  addItem: () => void;
+  removeItem: () => void;
+  reset: () => void;
+  updateFlexboxContainer: (state: Partial<FlexboxLayoutContainerState>) => void;
+  updateFlexboxItem: (
+    index: number,
+    propertyName: FlexboxItemProperty,
+    state: Partial<FlexboxItem>,
+  ) => void;
+}
+
+export type LayoutStore = LayoutState & LayoutActions;
+
+function getDefaultInitialState(): LayoutState {
+  return {
+    numItems: DEFAULT_NUM_ITEMS,
+    selectedItemIndex: DEFAULT_SELECTED_ITEM_INDEX,
+    flexDirection: FLEXBOX_DEFAULTS.flexDirection,
+    flexWrap: FLEXBOX_DEFAULTS.flexWrap,
+    justifyContent: FLEXBOX_DEFAULTS.justifyContent,
+    alignItems: FLEXBOX_DEFAULTS.alignItems,
+    order: Array.from({ length: DEFAULT_NUM_ITEMS }, () => FLEXBOX_DEFAULTS.order),
+    flexGrow: Array.from({ length: DEFAULT_NUM_ITEMS }, () => FLEXBOX_DEFAULTS.flexGrow),
+    flexShrink: Array.from({ length: DEFAULT_NUM_ITEMS }, () => FLEXBOX_DEFAULTS.flexShrink),
+    alignSelf: Array.from({ length: DEFAULT_NUM_ITEMS }, () => FLEXBOX_DEFAULTS.alignSelf),
+  } as const;
+}
+
+export function createLayoutStore() {
+  return createStore<LayoutStore>((set, get) => ({
+    ...getDefaultInitialState(),
+    selectItem: (index) => set({ selectedItemIndex: index }),
+    addItem: () => {
+      const { numItems, order, flexGrow, flexShrink, alignSelf } = get();
+
+      const newNumItems = numItems + 1;
+      const newOrder = [...order, FLEXBOX_DEFAULTS.order];
+      const newFlexGrow = [...flexGrow, FLEXBOX_DEFAULTS.flexGrow];
+      const newFlexShrink = [...flexShrink, FLEXBOX_DEFAULTS.flexShrink];
+      const newAlignSelf = [...alignSelf, FLEXBOX_DEFAULTS.alignSelf];
+
+      set({
+        numItems: newNumItems,
+        order: newOrder,
+        flexGrow: newFlexGrow,
+        flexShrink: newFlexShrink,
+        alignSelf: newAlignSelf,
+      });
+    },
+    removeItem: () => {
+      const { numItems, selectedItemIndex, order, flexGrow, flexShrink, alignSelf } = get();
+
+      if (selectedItemIndex === -1) {
+        return;
+      }
+
+      const newNumItems = numItems - 1;
+      const newOrder = [...order];
+      const newFlexGrow = [...flexGrow];
+      const newFlexShrink = [...flexShrink];
+      const newAlignSelf = [...alignSelf];
+
+      newOrder.splice(selectedItemIndex, 1);
+      newFlexGrow.splice(selectedItemIndex, 1);
+      newFlexShrink.splice(selectedItemIndex, 1);
+      newAlignSelf.splice(selectedItemIndex, 1);
+
+      set({
+        numItems: newNumItems,
+        selectedItemIndex: DEFAULT_SELECTED_ITEM_INDEX,
+        order: newOrder,
+        flexGrow: newFlexGrow,
+        flexShrink: newFlexShrink,
+        alignSelf: newAlignSelf,
+      });
+    },
+    reset: () => set(getDefaultInitialState()),
+    updateFlexboxContainer: (state) => set({ ...get(), ...state }),
+    updateFlexboxItem: (index, propertyName, state) => {
+      const oldState = get();
+
+      const newState: (number | FlexboxAlignSelf)[] = [...oldState[propertyName]];
+      const value = state[propertyName];
+
+      if (value === undefined) return;
+
+      newState.splice(index, 1, value);
+
+      set({ [propertyName]: newState });
+    },
+  }));
+}
+
+export type LayoutStoreApi = ReturnType<typeof createLayoutStore>;
