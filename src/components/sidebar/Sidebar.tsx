eb5065d5648451dfea827270e7fda490ff68d425
Javier Valero Cejudo <javalce29@gmail.com>
2024-02-14 19:33:32 +0100

    Remove unused imports and update sidebar layout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,13 +1,4 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { capitalizeFirstLetter } from '@/lib/utils';
-import { LAYOUT_TYPES } from '@/models/layout';
 import { AddItem } from './AddItem';
 import { FlexboxItemOptions } from './FlexboxItemOptions';
 import { FlexboxOptions } from './FlexboxOptions';
@@ -18,29 +9,15 @@ export function Sidebar() {
     <aside className='flex flex-col gap-y-5 p-2 [grid-area:options]'>
       <AddItem />
       <RemoveItem />
-      <section>
-        <Select defaultValue={LAYOUT_TYPES.FLEXBOX}>
-          <SelectTrigger className=''>
-            <SelectValue placeholder='Select a layout type' />
-          </SelectTrigger>
-          <SelectContent>
-            {Object.entries(LAYOUT_TYPES).map(([key, value]) => (
-              <SelectItem key={key} value={value}>
-                {capitalizeFirstLetter(value)}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-      </section>
       <Tabs defaultValue='container'>
         <TabsList className='grid w-full grid-cols-2'>
           <TabsTrigger value='container'>Container</TabsTrigger>
           <TabsTrigger value='item'>Item</TabsTrigger>
         </TabsList>
-        <TabsContent className='mt-10' value='container'>
+        <TabsContent className='mt-5' value='container'>
           <FlexboxOptions />
         </TabsContent>
-        <TabsContent className='mt-10' value='item'>
+        <TabsContent className='mt-5' value='item'>
           <FlexboxItemOptions />
         </TabsContent>
       </Tabs>
